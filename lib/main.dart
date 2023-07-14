153a76d231fe81284bd862bf7466e85703223232
ru-sparks <ru_sparks@yahoo.com>
2023-07-14 12:59:17 -0700

    Step 2: Refactored code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,188 +1,184 @@
 import 'package:flutter/material.dart';
-void main() => runApp( ECommerceScreen());
+
+void main() => runApp(const ECommerceScreen());
+
 class ECommerceScreen extends StatelessWidget {
   const ECommerceScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp (
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.purple,
-        appBar: AppBar(
-          backgroundColor: Colors.purpleAccent,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
-          leading: const Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Icon(Icons.home),
-          ),
-          title: const Text('Let\'s go shopping!'),
-          elevation: 0,
-          actions: const <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Icon(Icons.shopping_cart),
-            )
-          ],
-        ),
+        appBar: _buildAppBar(),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: <Widget>[
-              Row(
-                children: const <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      'Recommended',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      'Formal Wear',
-                      style: TextStyle(
-                        color: Colors.white54,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      'Casual Wear',
-                      style: TextStyle(
-                        color: Colors.white54,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              _buildToggleBar(),
               Image.asset('assets/woman_shopping.jpg'),
-              const SizedBox(height: 15),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      height: 80,
-                      decoration: BoxDecoration(
-                          color: Colors.orangeAccent,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: const Center(
-                          child: Text(
-                            'Best Sellers',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: Container(
-                      height: 80,
-                      decoration: BoxDecoration(
-                          color: Colors.indigoAccent,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: const Center(
-                          child: Text(
-                            'Daily Deals',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )),
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 15),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      height: 80,
-                      decoration: BoxDecoration(
-                          color: Colors.lightGreen,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: const Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Center(
-                          child: Text(
-                            'Must buy in summer',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: Container(
-                      height: 80,
-                      decoration: BoxDecoration(
-                          color: Colors.redAccent,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: const Center(
-                          child: Text(
-                            'Last Chance',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )),
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 15),
-              Container(
-                height: 200,
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/textiles.jpg',
-                      fit: BoxFit.fitHeight,
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const <Widget>[
-                            Text(
-                              'Lorem Ipsum',
-                            //  style: Theme.of(context).textTheme.title,
-                            ),
-                            Text(
-                                'Dolor sit amet, consectetur adipiscing elit. Quisque faucibus.')
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
+              const DealButtons(),
+              _buildProductTile(),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container _buildProductTile() {
+    return Container(
+              height: 200,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Image.asset(
+                    'assets/textiles.jpg',
+                    fit: BoxFit.fitHeight,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const <Widget>[
+                          Text(
+                            'Lorem Ipsum',
+                            //  style: Theme.of(context).textTheme.title,
+                          ),
+                          Text(
+                              'Dolor sit amet, consectetur adipiscing elit. Quisque faucibus.')
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+  }
+
+  Row _buildToggleBar() {
+    return Row(
+      children: const <Widget>[
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Recommended',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Formal Wear',
+            style: TextStyle(
+              color: Colors.white54,
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Casual Wear',
+            style: TextStyle(
+              color: Colors.white54,
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.purpleAccent,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+      leading: const Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Icon(Icons.home),
+      ),
+      title: const Text('Let\'s go shopping!'),
+      elevation: 0,
+      actions: const <Widget>[
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Icon(Icons.shopping_cart),
+        )
+      ],
+    );
+  }
+}
+
+class DealButtons extends StatelessWidget {
+  const DealButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 15),
+        Row(
+          children: const <Widget>[
+            DealButton(text: 'Best Sellers', color: Colors.orangeAccent),
+            SizedBox(width: 10),
+            DealButton(text: 'Daily Deals', color: Colors.indigoAccent),
+          ],
+        ),
+        const SizedBox(height: 15),
+        Row(
+          children: const <Widget>[
+            DealButton(text: 'Must buy in summer', color: Colors.lightGreen),
+            SizedBox(width: 10),
+            DealButton(text: 'Last Chance', color: Colors.redAccent),
+          ],
+        ),
+        const SizedBox(height: 15),
+      ],
+    );
+  }
+}
+
+class DealButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  const DealButton({
+    required this.text,
+    required this.color,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 80,
+        decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(20)),
+        child:  Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+             text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
       ),
